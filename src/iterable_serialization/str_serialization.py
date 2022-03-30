dd1522ce2497281ea5a2485c848b3f693319b815
Stefan Taubert <stefan.taubert@posteo.de>
2022-03-30 17:58:40 +0200

    added basic code

---
+++ src/iterable_serialization/str_serialization.py
@@ -0,0 +1,57 @@
+from typing import Generator, Iterable
+
+
+def can_serialize_symbols(symbols: Iterable[str], split_symbol: str) -> bool:
+  for symbol in symbols:
+    if split_symbol in symbol and symbol != split_symbol:
+      return False
+  return True
+
+
+def serialize_symbols(symbols: Iterable[str], split_symbol: str) -> str:
+  assert len(split_symbol) == 1
+  text = split_symbol.join(symbols)
+  return text
+
+
+def can_deserialize_symbols(text: str, split_symbol: str) -> bool:
+  no_of_subsequent_split_symbols = 1
+  last_char_was_non_split_symbol = False
+  for char in text:
+    if char == split_symbol:
+      no_of_subsequent_split_symbols += 1
+      last_char_was_non_split_symbol = False
+    else:
+      if no_of_subsequent_split_symbols % 2 == 0 and not last_char_was_non_split_symbol:
+        return False
+      no_of_subsequent_split_symbols = 0
+      last_char_was_non_split_symbol = True
+  if no_of_subsequent_split_symbols % 2 == 0:
+    return True
+  return False
+
+
+def deserialize_symbols(text: str, split_symbol: str) -> Generator[str, None, None]:
+  #assert can_deserialize(text, split_symbol)
+  assert len(split_symbol) == 1
+  symbol = ""
+  yield_subsequent_split_symbol = None
+  for char in text:
+    if char == split_symbol:
+      if symbol == "":
+        if yield_subsequent_split_symbol is None:
+          yield char
+          yield_subsequent_split_symbol = False
+        elif yield_subsequent_split_symbol:
+          yield char
+          yield_subsequent_split_symbol = False
+        else:
+          yield_subsequent_split_symbol = True
+      else:
+        yield symbol
+        symbol = ""
+        yield_subsequent_split_symbol = True
+    else:
+      symbol += char
+  if symbol != "":
+    yield symbol
